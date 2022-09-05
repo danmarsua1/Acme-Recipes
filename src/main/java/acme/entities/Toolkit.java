efb2621db8334e367eca09b6843eae097deb0e08
Bogdan Stefan <sbogdanm26@hotmail.com>
2022-09-05 16:23:51 +0100

    D02-14: UML done.

--- src/main/java/acme/entities/Toolkit.java
+++ src/main/java/acme/entities/Toolkit.java
@@ -2,13 +2,17 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -47,4 +51,9 @@ public class Toolkit extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+	
+	@Valid
+	@NotNull
+	@ManyToOne(optional = false)
+	protected Inventor inventor;
 }
