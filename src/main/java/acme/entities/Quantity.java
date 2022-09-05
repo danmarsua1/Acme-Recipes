14ab9b6ccbb60ed2dc408b8398a1785aa6f2ad35
luicersan <luicersan@alum.us.es>
2022-09-06 01:00:43 +0200

    D02-14: fix

--- src/main/java/acme/entities/Quantity.java
+++ src/main/java/acme/entities/Quantity.java
@@ -1,12 +1,11 @@
 package acme.entities;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.NotNull;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
-
 
 import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
@@ -41,8 +40,8 @@ public class Quantity extends AbstractEntity {
 	@ManyToOne(optional=false)
 	protected Item item;
 	
-	@NotNull
-	@Valid
-	@ManyToOne(optional = false)
-	protected Recipe recipe;
+//	@NotNull
+//	@Valid
+//	@ManyToOne(optional = false)
+//	protected Recipe recipe;
 }
