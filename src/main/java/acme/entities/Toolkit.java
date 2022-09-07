e88e29d5b7531d9ced300d6cd8b0b2af97f7641a
luicersan <luicersan@alum.us.es>
2022-09-07 10:26:54 +0200

    D04-18: fix ConfigurationUpdateTest

--- src/main/java/acme/entities/Toolkit.java
+++ src/main/java/acme/entities/Toolkit.java
@@ -2,17 +2,13 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
-import acme.roles.Inventor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -52,8 +48,8 @@ public class Toolkit extends AbstractEntity {
 
 	// Relationships ----------------------------------------------------------
 	
-	@Valid
-	@NotNull
-	@ManyToOne(optional = false)
-	protected Inventor inventor;
+//	@Valid
+//	@NotNull
+//	@ManyToOne(optional = false)
+//	protected Inventor inventor;
 }
