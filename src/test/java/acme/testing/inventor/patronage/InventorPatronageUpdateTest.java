235dcc47a6efb1cc66092cad7709d5d6a6ba684a
luicersan <luicersan@alum.us.es>
2022-09-08 18:29:10 +0200

    D04-18: fix InventorPatronageUpdateTest

--- src/test/java/acme/testing/inventor/patronage/InventorPatronageUpdateTest.java
+++ src/test/java/acme/testing/inventor/patronage/InventorPatronageUpdateTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.TestHarness;
 
-public class InventorPatronageUpdateTest extends TestHarness	{
+public class InventorPatronageUpdateTest extends TestHarness {
+	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/inventor/patronage/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String status, final String code,final String legalStuff,final String budget,final String creationMoment,final String initDate,final String finishDate, final String link) {
+	public void positiveTest(final int recordIndex, final String status, final String code,final String legalStuff, final String budget,
+		final String creationMoment, final String initDate, final String finishDate, final String link) {
+		
 		super.signIn("inventor1", "inventor1");
 		
 		super.clickOnMenu("Inventor", "Patronages");
@@ -20,13 +23,14 @@ public class InventorPatronageUpdateTest extends TestHarness	{
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
 		super.fillInputBoxIn("status", status);
+		
 		super.clickOnSubmit("Update");
 		
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 		super.checkColumnHasValue(recordIndex, 0, initDate);
 		super.checkColumnHasValue(recordIndex, 1, finishDate);
-//		super.checkColumnHasValue(recordIndex, 2, budget);
+		super.checkColumnHasValue(recordIndex, 2, budget);
 		super.checkColumnHasValue(recordIndex, 3, status);
 		
 		super.clickOnListingRecord(recordIndex);
@@ -34,8 +38,8 @@ public class InventorPatronageUpdateTest extends TestHarness	{
 		super.checkInputBoxHasValue("code", code);
 		super.checkInputBoxHasValue("creationMoment", creationMoment);
 		super.checkInputBoxHasValue("initDate", initDate);
-//		super.checkInputBoxHasValue("finishDate", finishDate);
-//		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("finishDate", finishDate);
+		super.checkInputBoxHasValue("budget", budget);
 		super.checkInputBoxHasValue("status", status);
 		super.checkInputBoxHasValue("legalStuff", legalStuff);
 		super.checkInputBoxHasValue("link", link);
