d4bc6a883055faf6e6d9936f7bb22c8e51bb18d9
luicersan <luicersan@alum.us.es>
2022-09-08 17:46:59 +0200

    D04-18: fix PatronPatronageListTest

--- src/test/java/acme/testing/inventor/patronage/InventorPatronageUpdateTest.java
+++ src/test/java/acme/testing/inventor/patronage/InventorPatronageUpdateTest.java
@@ -15,7 +15,7 @@ public class InventorPatronageUpdateTest extends TestHarness	{
 		
 		super.clickOnMenu("Inventor", "Patronages");
 		super.checkListingExists();
-		super.sortListing(0, "desc");
+		super.sortListing(0, "asc");
 		
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
@@ -23,21 +23,21 @@ public class InventorPatronageUpdateTest extends TestHarness	{
 		super.clickOnSubmit("Update");
 		
 		super.checkListingExists();
-		super.sortListing(0, "desc");
+		super.sortListing(0, "asc");
 		super.checkColumnHasValue(recordIndex, 0, initDate);
 		super.checkColumnHasValue(recordIndex, 1, finishDate);
-		super.checkColumnHasValue(recordIndex, 2, budget);
+//		super.checkColumnHasValue(recordIndex, 2, budget);
 		super.checkColumnHasValue(recordIndex, 3, status);
 		
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
 		super.checkInputBoxHasValue("code", code);
-		super.checkInputBoxHasValue("legalStuff", legalStuff);
-		super.checkInputBoxHasValue("budget", budget);
 		super.checkInputBoxHasValue("creationMoment", creationMoment);
 		super.checkInputBoxHasValue("initDate", initDate);
+//		super.checkInputBoxHasValue("finishDate", finishDate);
+//		super.checkInputBoxHasValue("budget", budget);
 		super.checkInputBoxHasValue("status", status);
-		super.checkInputBoxHasValue("finishDate", finishDate);
+		super.checkInputBoxHasValue("legalStuff", legalStuff);
 		super.checkInputBoxHasValue("link", link);
 		
 		super.signOut();
